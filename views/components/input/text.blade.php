2746c1e17a3d900e14a6ca557967ba6679b1a521
rashidhamidov <resid.hemidov.2014@gmail.com>
2023-04-22 13:30:03 +0400

    form action changes 5

--- views/components/input/text.blade.php
+++ views/components/input/text.blade.php
@@ -1,5 +1,5 @@
 <input type="{{$field["type"]}}" id="id_{{$field["name"]}}" name="{{$field["name"]}}"
-       class="{{$field["className"]}}"
+       class="{{$field["className"]}}" step="any"
        value="@if($field["defaultValue"]) {{$field["defaultValue"]}} @endif
                    @if($data) {{$data[$field["name"]]}} @endif @if(old($field["name"])) {{old($field["name"])}} @endif"
        @if($field["required"]) required @endif
