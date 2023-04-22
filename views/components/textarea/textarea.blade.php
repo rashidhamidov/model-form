9a624cdb916a2f8362cc0112068a27a0b5f4078b
rashidhamidov <resid.hemidov.2014@gmail.com>
2023-04-22 10:32:02 +0400

    Form Width Commit

--- views/components/textarea/textarea.blade.php
+++ views/components/textarea/textarea.blade.php
@@ -1,9 +1,6 @@
 <div class="col-md-6 mb-3">
     <label for="id_{{$field["name"]}}" class="form-label text-capitalize">@lang($name)</label>
     <textarea name="{{$field["name"]}}" id="id_{{$field["name"]}}" class="{{$field["className"]}}"
-              @if($field["required"]) required @endif >@if(old($field["name"]))
-            {{old($field["name"])}}
-        @elseif($data)
-            {{$data[$field["name"]]}}
-        @endif</textarea>
+              @if($field["required"]) required @endif
+    >@if(old($field["name"])){{old($field["name"])}}@elseif($data){{$data[$field["name"]]}}@endif</textarea>
 </div>
