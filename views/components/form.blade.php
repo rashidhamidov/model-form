ed31ff747eb9303428bcb0e2675b8e2afaaba697
rashidhamidov <resid.hemidov.2014@gmail.com>
2023-04-22 09:39:41 +0400

    Form Width Commit

--- views/components/form.blade.php
+++ views/components/form.blade.php
@@ -10,41 +10,43 @@
         @method('PUT')
     @endif
     @csrf
-    @foreach($fields as $name=>$field)
-        @if($field["element"]=="input")
-            <div class="col-md-6 mb-3">
-                <label for="id_{{$field["name"]}}" class="form-label text-capitalize">@lang("form.".$name)</label>
-                @if($field["type"]=="checkbox")
-                    @include('model-form::components.input.checkbox')
-                @elseif($field["type"]=="radio")
-                    @include('model-form::components.input.radio')
-                @elseif($field["type"]=="file")
-                    @include('model-form::components.input.file')
-                    @if($data)
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img class="w-50" src="{{$field["defaultValue"]}}">
-                            </div>
-                        </div>
-                    @endif
-                @else
-                    @include('model-form::components.input.text')
+        <div class="row">
+            @foreach($fields as $name=>$field)
+                @if($field["element"]=="input")
+                    <div class="col-md-6 mb-3">
+                        <label for="id_{{$field["name"]}}" class="form-label text-capitalize">@lang("form.".$name)</label>
+                        @if($field["type"]=="checkbox")
+                            @include('model-form::components.input.checkbox')
+                        @elseif($field["type"]=="radio")
+                            @include('model-form::components.input.radio')
+                        @elseif($field["type"]=="file")
+                            @include('model-form::components.input.file')
+                            @if($data)
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img class="w-50" src="{{$field["defaultValue"]}}">
+                                    </div>
+                                </div>
+                            @endif
+                        @else
+                            @include('model-form::components.input.text')
+                        @endif
+                        @error($field["name"])
+                        <span class="text-danger">{{$message}}</span>
+                        @enderror
+                    </div>
                 @endif
-                @error($field["name"])
-                <span class="text-danger">{{$message}}</span>
-                @enderror
-            </div>
-        @endif
-        @if($field["element"]=="textarea")
-            @include('model-form::components.textarea.textarea')
-        @endif
-        @if($field["element"]=="select")
-            @include('model-form::components.select.select')
-        @endif
-        @if($field["element"]=="foreign")
-            @include('model-form::components.select.foreign')
-        @endif
+                @if($field["element"]=="textarea")
+                    @include('model-form::components.textarea.textarea')
+                @endif
+                @if($field["element"]=="select")
+                    @include('model-form::components.select.select')
+                @endif
+                @if($field["element"]=="foreign")
+                    @include('model-form::components.select.foreign')
+                @endif
 
-    @endforeach
+            @endforeach
+        </div>
     <button type="submit" class="btn btn-primary">@lang('Send')</button>
 </form>
--- views/components/textarea/textarea.blade.php
+++ views/components/textarea/textarea.blade.php
@@ -1,4 +1,4 @@
-<div class="mb-3">
+<div class="col-md-6 mb-3">
     <label for="id_{{$field["name"]}}" class="form-label text-capitalize">@lang($name)</label>
     <textarea name="{{$field["name"]}}" id="id_{{$field["name"]}}" class="{{$field["className"]}}"
               @if($field["required"]) required @endif >@if(old($field["name"]))
